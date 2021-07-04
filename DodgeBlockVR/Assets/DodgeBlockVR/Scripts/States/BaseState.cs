c6c2f79e4aeb0be40c4ae2f96288559786e5434f
DESKTOP-97TO8KI\James <jamesmcneil42@hotmail.co.uk>
2021-07-04 22:15:44 +0100

    Add a concept of a score and lives - not yet messaged to the user

--- DodgeBlockVR/Assets/DodgeBlockVR/Scripts/States/BaseState.cs
+++ DodgeBlockVR/Assets/DodgeBlockVR/Scripts/States/BaseState.cs
@@ -8,19 +8,30 @@ using UnityEngine;
 
 public class BaseState : FlowStateBase
 {
+    const int k_startingPlayerLives = 3;
+
     private BlockSystem m_blockSystem = null;
     private PlayerColliderMono m_playerCollider = null;
+    private PlayerData m_playerData;
 
     protected override void StartPresentingState()
     {
         m_playerCollider = Object.FindObjectOfType<PlayerColliderMono>();
         m_blockSystem = new BlockSystem(m_playerCollider.transform);
+        m_playerData = new PlayerData() { Lives = k_startingPlayerLives, Score = 0 };
     }
 
     protected override void UpdateActiveState()
     {
         float dt = Time.deltaTime;
         m_blockSystem.Update(dt);
+
+        //TODO: Remove if check since we'll be in a different state if are out of lives
+        if(m_playerData.Lives > 0)
+        {
+            //TODO: Add a score manager that adds set amount over X seconds instead of per frame
+            ++m_playerData.Score;
+        }
     }
 
     protected override void FixedUpdateActiveState()
@@ -30,7 +41,13 @@ public class BaseState : FlowStateBase
             var colliderObjs = m_playerCollider.ConsumeCollisions();
             foreach(GameObject go in colliderObjs)
             {
-                Debug.Log("You got hit");
+                --m_playerData.Lives;
+            }
+
+            if(m_playerData.Lives <= 0)
+            {
+                //TODO: Game over state.
+                Debug.Log($"Game Over. You scored: {m_playerData.Score}");
             }
         }
     }
